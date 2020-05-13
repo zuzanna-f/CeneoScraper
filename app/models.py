5767d9ffaef570faad8abb69a4dbc1b7a5ae3121
zuzanna-f <zflo002@gmail.com>
2020-05-13 14:18:07 +0200

    ZF 2020-05-13

--- app/models.py
+++ app/models.py
@@ -1,48 +1,96 @@
-class Product:
+#import bibliotek
+import requests
+from bs4 import BeautifulSoup
+from enum import Enum, auto
+
+class Product():
     def __init__(self, product_id=None, name=None, opinions=[]):
         self.product_id = product_id
         self.name = name
         self.opinions = opinions
+    
+    def extract_product(self):
+        page_response = requests.get("https://www.ceneo.pl/"+self.product_id)
+        page_tree = BeautifulSoup(page_response.text, 'html.parser')
+        self.name = page_tree.select("h1.product-name").pop().get_text().strip()
+        try:
+            opinions_count = int(page_tree.select("a.product-reviews-link > span").pop().get_text().strip())
+        except IndexError:
+            opinions_count = 0
+        if opinions_count > 0:
+            url_prefix = "https://www.ceneo.pl"
+            url_postfix = "#tab=reviews"
+            url = url_prefix+"/"+self.product_id+url_postfix
+            while url:
+                #pobranie kodu HTML strony z adresu URL
+                page_response = requests.get(url)
+                page_tree = BeautifulSoup(page_response.text, 'html.parser')
 
+                #wybranie z kodu strony fragmentów odpowiadających poszczególnym opiniom
+                opinions = page_tree.select("li.js_product-review")
+                
+                #ekstrakcja składowyh dla pojedynczej opinii z listy
+                for opinion in opinions: 
+                    op = Opinion()
+                    op.extract_opinion(opinion)
+                    self.opinions.append(op)
+                try:
+                    url = url_prefix+page_tree.select("a.pagination__next").pop()["href"]
+                except IndexError:
+                    url = None
 
 class Opinion:
     #lista składowych opinii wraz z selektorami i atrybutami
     selectors = {
-        "author":['div.reviewer-name-line'],
+        "author": ['div.reviewer-name-line'],
         "recommendation":['div.product-review-summary > em'],
         "stars":['span.review-score-count'],
-        "content":['p.product-review-body'],
-        "pros":['div.pros-cell > ul'],
-        "cons":['div.cons-cell > ul'],
-        "useful":['button.vote-yes', 'data-total-vote'],
-        "useless":['button.vote-no', 'data-total-vote'],
+        "content": ['p.product-review-body'],
+        "pros": ['div.pros-cell > ul'],
+        "cons":['div.cons-cell > ul'], 
+        "useful":['button.vote-yes', "data-total-vote"],
+        "useless":['button.vote-no', "data-total-vote"],
         "purchased":['div.product-review-pz'],
-        "purchase_date":['span.review-time > time:nth-of-type(1)', "datetime"],
-        "review_date":['span.review-time > time:nth-of-type(2)', "datetime"]
-    } 
-
-    #jonstruktor (inicjalizator) obiektu klasy
+        "purchase_date":['span.review-time > time:nth-of-type(1)',"datetime"],
+        "review_date":['span.review-time > time:nth-of-type(2)',"datetime"]
+    }
+    #konstruktor (inincjalizator) obiektu klasy
     def __init__(self, opinion_id=None, author=None, recommendation=None, stars=None, content=None, 
-                pros=None, cons=None, useful=None, useless=None, purchased=None, purchase_date=None, review_date=None)
-            self.opinion_id = opinion_id
-            self.author = author
-            self.recommendation = recommendation
-            self.stars = stars
-            self.content = content
-            self.pros = pros
-            self.cons = cons
-            self.useful = useful
-            self.useless = useless
-            self.purchased = purchased
-            self.purchase_date = purchase_date
-            self.review_date = review_date
-        # reprezntacja tekstowa obiektu klasy
+                pros=None, cons=None, useful=None, useless=None, purchased=None, purchase_date=None, review_date=None):
+        self.opinion_id = opinion_id
+        self.author = author
+        self.recommendation = recommendation
+        self.stars = stars
+        self.content = content
+        self.pros = pros
+        self.cons = cons
+        self.useful = useful
+        self.useless = useless
+        self.purchased = purchased
+        self.purchase_date = purchase_date
+        self.review_date = review_date
+    # reprezentacja tekstowa obiektu klasy
     def __str__(self):
-        return f'opinion id: {self.opinion_id}\n author {self.author}\n'
-    
-    def extract_opinion(self):
-        pass
-        
+        return f'opinion id: {self.opinion_id}\n author: {self.author}\n'
 
-opinion = Opinion()
-print(opinion)
+    #reprezentacja słownikowa obiektu
+    def __repr__(self):
+        pass
+    #
+    def extract_opinion(self, opinion):
+        features = {key:extract_feature(opinion, *args)
+                    for key, args in selectors.items()}
+        self.opinion_id = int(opinion["data-entry-id"])
+        pass
+    def transform_opinion(self):
+        features["purchased"] = True if features["purchased"] == "Opinia potwierdzona zakupem" else False
+        features["useful"] = int(features["useful"])
+        features["useless"] = int(features["useless"])
+        features["content"] = remove_whitespaces(features["content"])
+        features["pros"] = remove_whitespaces(features["pros"])
+        features["cons"] = remove_whitespaces(features["cons"])
+        pass
+
+
+product = Product("92745077")
+product.extract_product()
--- app/routes.py
+++ app/routes.py
@@ -2,8 +2,9 @@ from app import app
 from flask import render_template, request
 from flaskext.markdown import Markdown
 from app.forms import ProductForm
-
-app.config['SECRET_KEY'] = "Klucz"
+from app.models import Product, Opinion
+import requests
+app.config['SECRET_KEY'] = "Tajemniczy_mysi_sprzęt"
 
 Markdown(app)
 
@@ -23,7 +24,14 @@ def about():
 def extract():
     form = ProductForm()
     if form.validate_on_submit():
-        return "Przesłano formularz"
+        page_response = requests.get("https://www.ceneo.pl/"+request.form['product_code'])
+        if  page_response.status_code == 200:
+            product = Product(request.form['product_code'])
+            product.extract_product()
+            return "OK"
+        else:
+            form.product_code.errors.append("Dla podanego kodu nie ma produktu")
+            return render_template("extract.html", form=form)
     return render_template("extract.html", form=form)
 
 @app.route('/products')
@@ -32,8 +40,8 @@ def products():
 
 @app.route('/product/<product_id>')
 def product():
-    pass  
+    pass
 
 @app.route('/analyzer/<product_id>')
 def analyzer():
-    return 'Podaj kod produktu do analizy'
+    return "Podaj kod produktu do analizy"
